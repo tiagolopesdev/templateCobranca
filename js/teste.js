1a090a81dd53806a2e2f5bd9e9c32079186f1475
Tiago Lopes <saxtiago14@gmail.com>
2022-08-02 16:43:38 -0300

    Script para pegar click e exibir opções do menu.

--- js/teste.js
+++ js/teste.js
@@ -1,3 +1,9 @@
-$('#btn-mensage').click(function () {
-    $('#staticBackdrop').modal();
+let show = true;
+
+const menuSection = document.querySelector(".menuSection");
+const menuToggle =  menuSection.querySelector(".menuToggle");
+
+menuToggle.addEventListener("click", () => {    
+    menuSection.classList.toggle("on", show);
+    show = !show;
 })
