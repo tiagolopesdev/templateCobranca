cb6791332ffea82c8c8543bf3c0e68fb6fc4cd70
Tiago Lopes <saxtiago14@gmail.com>
2022-09-07 10:14:19 -0300

    Done - implementation JS to view the search filed.

--- js/teste.js
+++ js/teste.js
@@ -1,11 +1,25 @@
-let show = true;
+let showOptions = true;
 
 const menuSection = document.querySelector(".menuSection");
 const menuToggle =  menuSection.querySelector(".menuToggle");
 const bottons = document.querySelector(".bottons");
 
 menuToggle.addEventListener("click", () => { 
-    document.body.style.overflow = show ? "hidden" : "initial";
-    menuSection.classList.toggle("on", show);
-    show = !show;
+    document.body.style.overflow = showOptions ? "hidden" : "initial";
+    menuSection.classList.toggle("on", showOptions);
+    showOptions = !showOptions;
+})
+
+let showSearch = true;
+
+const searchNav = document.querySelector(".searchNav");
+const boxSearch = document.querySelector(".boxSearch");
+const nameApplication = document.querySelector(".nameApplication");
+
+boxSearch.addEventListener("click", () => {
+    //document.body.style.overflow = showSearch ? "hidden" : initial;
+    console.log("Click");
+    searchNav.classList.toggle("on", showSearch);
+    nameApplication.classList.toggle("off", showSearch);
+    //showSearch = !showSearch;
 })
